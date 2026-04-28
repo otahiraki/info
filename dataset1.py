2ef16f526f1f671a4c0c697b0fbeeccb899fcee1
ota hiraki <e245733@ie.u-ryukyu.ac.jp>
2026-04-28 10:04:56 +0900

    1.2

--- dataset1.py
+++ dataset1.py
@@ -1,18 +1,35 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 # 真の関数
 def true_function(x):
     return np.sin(np.pi * x * 0.8) * 10
 
-if __name__ == "__main__":
-    print(true_function(np.array([0])))
+# データセット作成
+def create_dataset(n=20, seed=0):
+    np.random.seed(seed)
 
-if __name__ == "__main__":
-    x = np.linspace(-1, 1, 100)
+    x = np.random.uniform(-1, 1, n)
     y = true_function(x)
 
-    plt.plot(x, y, label="true function")
+    df = pd.DataFrame({
+        "観測点": x,
+        "真値": y
+    })
+
+    return df
+
+# 実行
+if __name__ == "__main__":
+    x_line = np.linspace(-1, 1, 100)
+    y_line = true_function(x_line)
+
+    df = create_dataset()
+
+    plt.plot(x_line, y_line, label="true function")
+    plt.scatter(df["観測点"], df["真値"], color="red", label="samples")
+
     plt.legend()
-    plt.savefig("ex1.1.png")
+    plt.savefig("ex1.2.png")
     plt.show()
